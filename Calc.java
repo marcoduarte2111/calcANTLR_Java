920d285da7aaa3bc71579a761d0c8da18c080547
marcoduarte2111 <juan.marcoduarte@gmail.com>
2024-08-25 12:48:26 -0500

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,26 @@
+/***
+ * Excerpted from "The Definitive ANTLR 4 Reference",
+***/
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Calc {
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if (args.length > 0) inputFile = args[0];
+        InputStream is = System.in;
+        if (inputFile != null) is = new FileInputStream(inputFile);
+
+        CharStream input = CharStreams.fromStream(is);  // Updated from ANTLRInputStream to CharStreams
+        LabeledExprLexer lexer = new LabeledExprLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LabeledExprParser parser = new LabeledExprParser(tokens);
+        ParseTree tree = parser.prog(); // parse
+
+        EvalVisitor eval = new EvalVisitor();
+        eval.visit(tree);
+    }
+}
